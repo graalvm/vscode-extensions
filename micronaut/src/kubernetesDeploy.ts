dfaf5594f2e7a7b1e56e36f70b053e7930b17f5f
Jan Horvath <jan@horvath.cz>
2021-10-05 10:33:42 +0200

    [GR-32083] wait to finish deployment before run

--- micronaut/src/kubernetesDeploy.ts
+++ micronaut/src/kubernetesDeploy.ts
@@ -11,28 +11,32 @@ import * as kubernetes from 'vscode-kubernetes-tools-api';
 import { askToExecCommand, createWrapper, findResourceFileByKind } from "./kubernetesUtil";
 import { kubernetesChannel } from './kubernetesChannel';
 
+const MAX_WAIT_CYCLES = 60;
+const WAIT_TIMEOUT = 500; //ms
+
 export async function deployProject() {
     const kubectl: kubernetes.API<kubernetes.KubectlV1> = await kubernetes.extension.kubectl.v1;
     if (!kubectl.available) {
         vscode.window.showInformationMessage("kubectl not found");
         return;
     }
-    
-    let wrapper =  await createWrapper();
-    kubernetesChannel.clearAndShow();
-    wrapper.buildAll()
-        .then(() => deploy())
-        .catch((error) => kubernetesChannel.appendLine(error));
-}
-
-export async function deploy() {
     const deploymentFile = await findResourceFileByKind('Deployment');
     if (!deploymentFile) {
         askToExecCommand(
             'extension.micronaut.createDeploy',
             'Deployment file is not present. Would you like to create it?');
-        return Promise.reject();
+        return;
     }
+    
+    let wrapper =  await createWrapper();
+    kubernetesChannel.clearAndShow();
+    wrapper.buildAll()
+        .then(() => deploy(deploymentFile))
+        .catch((error) => kubernetesChannel.appendLine(error));
+}
+
+export async function deploy(deploymentFile: string) {
+    
     const kubectl: kubernetes.API<kubernetes.KubectlV1> = await kubernetes.extension.kubectl.v1;
     if (kubectl.available) {
         let command = `get -f ${deploymentFile} -o jsonpath='{.metadata.name}'`;
@@ -44,11 +48,21 @@ export async function deploy() {
         } else {
             command = `apply -f ${deploymentFile}`;
         }
+        let oldRs = deploymentName ? await getLatestRs(kubectl.api, deploymentName) : undefined;
         kubernetesChannel.appendLine(`> kubectl ${command}`);
         result = await kubectl.api.invokeCommand(command);
         if (result) {
             if (result.code == 0) {
                 kubernetesChannel.appendLine(result.stdout);
+                if (deploymentName && oldRs) {
+                    let repeat = MAX_WAIT_CYCLES;
+                    while (oldRs == await getLatestRs(kubectl.api, deploymentName) && repeat-- > 0) {
+                        await new Promise(resolve => setTimeout(resolve, WAIT_TIMEOUT));
+                    }
+                    if (repeat > -1) {
+                        kubernetesChannel.appendLine(`APPLICATION DEPLOYED`);
+                    }
+                }
                 return Promise.resolve();
             } else {
                 return Promise.reject(result.stderr);
@@ -56,5 +70,13 @@ export async function deploy() {
         }
         return Promise.reject();
     }
+}
 
+async function getLatestRs(kubectl: kubernetes.KubectlV1, label: string) {
+    const command = `get rs --selector=app=${label} --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.name}'`;
+    let result = await kubectl.invokeCommand(command);
+    if (result?.code === 0) {
+        return result.stdout;
+    }
+    return undefined;
 }
--- micronaut/src/kubernetesRun.ts
+++ micronaut/src/kubernetesRun.ts
@@ -32,66 +32,67 @@ export async function runProject() {
     }
 
     wrapper.buildAll()
-    .then(() => deploy())
+    .then(() => deploy(deploymentFile))
     .then(() => run(proj.name, deploymentFile))
-    .catch((error) => kubernetesChannel.appendLine(error));
+    .catch((error) => {
+        if (error) {
+            kubernetesChannel.appendLine(error)
+        }
+    });
     
-    }
+}
         
-    async function run(name: string, resourceFile: string) {
-        const kubectl: kubernetes.API<kubernetes.KubectlV1> = await kubernetes.extension.kubectl.v1;
-            if (!kubectl.available) {
-                vscode.window.showErrorMessage(`kubectl not available: ${kubectl.reason}.`);
-                return;
-            }
-        let rl = readline.createInterface({
-            input: fs.createReadStream(resourceFile),
-        })
-        let ports = [];
-        for await (const line of rl) {
-            let matches = line.match(/\s*containerPort:\s+(\d+)/);
-            if (matches) {
-                ports.push(matches[1]);
-            }
-        }
-        let port: number | undefined;
-        if (ports.length > 1) {
-            port = Number(await vscode.window.showQuickPick(ports));
-        } else if (ports.length == 1) {
-            port = Number(ports[0]);
-        }
-        if (port === undefined) {
-            vscode.window.showInformationMessage(`port ${port}`);
+async function run(name: string, resourceFile: string) {
+    const kubectl: kubernetes.API<kubernetes.KubectlV1> = await kubernetes.extension.kubectl.v1;
+        if (!kubectl.available) {
+            vscode.window.showErrorMessage(`kubectl not available: ${kubectl.reason}.`);
             return;
         }
-        let command = `wait --for=condition=available deployment ${name}`;
-        await kubectl.api.invokeCommand(command);
-
-        command = `get pods --selector=app=${name} -o jsonpath='{..items[*].metadata.name}'`
-        let result = await  kubectl.api.invokeCommand(command);
-        let pods: string[] = [];
-        if (result && result.code == 0) {
-            let parts = result.stdout.split(' ');
-            parts.forEach(pod => {
-                pods.push(pod);
-            });
+    let rl = readline.createInterface({
+        input: fs.createReadStream(resourceFile),
+    })
+    let ports = [];
+    for await (const line of rl) {
+        let matches = line.match(/\s*containerPort:\s+(\d+)/);
+        if (matches) {
+            ports.push(matches[1]);
         }
-        if (port && pods.length > 0) {
-            command = `wait --for=condition=ready pod ${pods[0]}`;
-            await kubectl.api.invokeCommand(command);
-            setTimeout(() => {}, 2000);
-            forwardSession = await kubectl.api.portForward(
-                pods[0], 
-                undefined, 
-                port, 
-                port, 
-                { showInUI: { location: 'status-bar' } 
-            });
-            if (!forwardSession) {
-                await vscode.window.showErrorMessage(`Can't access ${pods[0]} on cluster`);
-                return;
-            }
-            kubernetesChannel.appendLine(`You can access ${pods[0]} on http://localhost:${port}`);
-        }
-            
     }
+    let port: number | undefined;
+    if (ports.length > 1) {
+        port = Number(await vscode.window.showQuickPick(ports));
+    } else if (ports.length == 1) {
+        port = Number(ports[0]);
+    }
+    if (port === undefined) {
+        vscode.window.showInformationMessage(`port ${port}`);
+        return;
+    }
+
+    let command = `get pods --selector=app=${name} -o jsonpath='{..items[*].metadata.name}'`
+    let result = await  kubectl.api.invokeCommand(command);
+    let pods: string[] = [];
+    if (result && result.code == 0) {
+        let parts = result.stdout.split(' ');
+        parts.forEach(pod => {
+            pods.push(pod);
+        });
+    }
+    if (port && pods.length > 0) {
+        command = `wait --for=condition=ready pod ${pods[0]}`;
+        await kubectl.api.invokeCommand(command);
+        forwardSession = await kubectl.api.portForward(
+            pods[0], 
+            undefined, 
+            port, 
+            port, 
+            { showInUI: { location: 'status-bar' } 
+        });
+        if (!forwardSession) {
+            await vscode.window.showErrorMessage(`Can't access ${pods[0]} on cluster`);
+            return;
+        }
+        kubernetesChannel.appendLine(`You can access ${pods[0]} on http://localhost:${port}`);
+    }
+        
+}
