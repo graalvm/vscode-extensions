c615f3bc3fa772a83a16d7bcb4b44a13678f4e15
Ondřej Douda <ondrej.douda@oracle.com>
2022-08-22 14:43:33 +0200

    Development build speedup.

--- graalvm/webpack.config.js
+++ graalvm/webpack.config.js
@@ -5,7 +5,7 @@
 const path = require('path');
 const maven = require('maven');
 const CopyPlugin = require("copy-webpack-plugin");
-        
+
 
 /**@type {import('webpack').Configuration}*/
 const config = {
@@ -16,7 +16,8 @@ const config = {
             apply: (compiler) => {
                 compiler.hooks.beforeCompile.tapPromise('MavenPlugin', (compilation) => {
                     const mvn = maven.create({ cwd: '../nbcode-graalvm' });
-                    return mvn.execute(['clean', 'install'], {'skipTests': true});}
+                    return mvn.execute(['clean', 'install'], { 'skipTests': true });
+                }
                 );
             }
         },
@@ -56,10 +57,77 @@ const config = {
             exclude: /node_modules/,
             include: path.resolve(__dirname, 'src'),
             use: [{
-                loader: 'ts-loader',
+                loader: 'ts-loader'
             }]
         }]
     },
 }
+const devConf = {
+    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-module.exports = config;
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.beforeCompile.tapPromise('MavenPlugin', (compilation) => {
+                    const mvn = maven.create({ cwd: '../nbcode-graalvm' });
+                    return mvn.execute(['clean', 'install'], { 'skipTests': true });
+                }
+                );
+            }
+        },
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "../nbcode-graalvm/nbcode-graalvm-ojdbc/target/nbm/netbeans/nbcodegraalvm/",
+                    to: "../nbcode/graalvmextra/"
+                }
+            ],
+        }),
+    ],
+
+    entry: {
+        extension: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+        debug: './src/graalVMDebugAdapter.ts'
+    },
+    output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate",
+    },
+    resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js'],
+        symlinks: false,
+        cacheWithContext: false
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            include: path.resolve(__dirname, 'src'),
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true, // https://github.com/TypeStrong/ts-loader#faster-builds
+                }
+            }]
+        }]
+    },
+}
+// https://webpack.js.org/configuration/mode/#mode-none
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        return devConf;
+    }
+
+    if (argv.mode === 'production') {
+        return config;
+    }
+    return config;
+};
--- micronaut/webpack.config.js
+++ micronaut/webpack.config.js
@@ -29,10 +29,54 @@ const config = {
             test: /\.ts$/,
             exclude: /node_modules/,
             use: [{
+                loader: 'ts-loader'
+            }]
+        }]
+    },
+}
+const devConf = {
+    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+
+    entry: {
+        extension: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    },
+    output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js'],
+        symlinks: false,
+        cacheWithContext: false
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: [{
                 loader: 'ts-loader',
+                options: {
+                    transpileOnly: true, // https://github.com/TypeStrong/ts-loader#faster-builds
+                }
             }]
         }]
     },
 }
 
-module.exports = config;
+// https://webpack.js.org/configuration/mode/#mode-none
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        return devConf;
+    }
+
+    if (argv.mode === 'production') {
+        return config;
+    }
+    return config;
+};
