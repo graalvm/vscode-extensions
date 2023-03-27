93024bac266c1ac9c9a79cdb68ecc61ddb572cb4
Ondřej Douda <ondrej.douda@oracle.com>
2023-03-27 19:42:04 +0200

    [GR-34472] Don't touch user specified Proxy support.

--- graalvm/src/graalVMConfiguration.ts
+++ graalvm/src/graalVMConfiguration.ts
@@ -138,7 +138,6 @@ export async function setupProxy() {
             }
             if (proxy !== out || mavenProxy !== out) {
                 await http.update('proxy', out, true);
-                await http.update('proxySupport', out ? 'on' : 'off', true);
                 await vscode.commands.executeCommand('extension.graalvm.refreshInstallations');
                 if (isMvn && mavenProxy !== out) {
                     utils.askYesNo(`Change also Maven proxy in "${getMavenSettingsFilePath()}"?`,
